2b665940328e78bae9d6d459c4dd12e09ddd7f4f
nomadhailey <blessedgene11@gmail.com>
2021-04-20 22:56:47 +0900

    feat: workspace

--- alecture_practice/layouts/App/index.tsx
+++ alecture_practice/layouts/App/index.tsx
@@ -5,6 +5,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
 const Channel = loadable(() => import('@pages/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/login" component={LogIn} />
       <Route path="/signup" component={SignUp} />
       <Route path="/workspace/channel" component={Channel} />
+      <Route path="/workspace/dm" component={DirectMessage} />
     </Switch>
   );
 };
--- alecture_practice/layouts/Workspace/index.tsx
+++ alecture_practice/layouts/Workspace/index.tsx
@@ -1,8 +1,20 @@
+import {
+  Channels,
+  Chats,
+  Header,
+  MenuScroll,
+  ProfileImg,
+  RightMenu,
+  WorkspaceName,
+  Workspaces,
+  WorkspaceWrapper,
+} from '@layouts/Workspace/styles';
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
 import React, { FC, useCallback } from 'react';
 import { Redirect } from 'react-router';
 import useSWR from 'swr';
+import gravatar from 'gravatar';
 
 const Workspace: FC = ({ children }) => {
   const { data, error, revalidate, mutate } = useSWR('/api/users', fetcher);
@@ -23,8 +35,23 @@ const Workspace: FC = ({ children }) => {
   }
   return (
     <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.nickname, { s: '28px', d: 'retro' })} alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
       <button onClick={onLogout}>로그아웃</button>
-      {children}
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>MenuScroll</MenuScroll>
+        </Channels>
+        <Chats>Chats</Chats>
+        {children}
+      </WorkspaceWrapper>
     </div>
   );
 };
